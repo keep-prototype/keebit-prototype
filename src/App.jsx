6bf1d276e41fe9eb005bdae12e319004d00a489c
ganggyunggyu <qwzx16@naver.com>
2025-03-10 16:42:32 +0900

    feat: 로딩 컴포넌트 뷰

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import './App.css';
 import { Home } from './pages/Home';
 import { More } from './widgets/More';
 import { Header } from './widgets/Header';
+import { FirstLoading } from './components/FirstLoading';
 
 function App() {
   const [isMore, setIsMore] = React.useState(false);
+
   const toggleMore = () => {
     setIsMore(!isMore);
   };
+
   return (
     <React.Fragment>
+      <FirstLoading />
       <Header isMore={isMore} toggleMore={toggleMore} />
       <Home />
       {isMore && <More />}
