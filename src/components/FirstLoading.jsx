3ff777988c2810ebe0fa5f17f50e6cb12772df91
ganggyunggyu <qwzx16@naver.com>
2025-03-10 16:41:51 +0900

    feat: 로딩 컴포넌트 제작

---
+++ src/components/FirstLoading.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export const FirstLoading = () => {
+  const [isFirst, setIsFirst] = React.useState(true);
+  const [isAnimate, setIsAnimate] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsAnimate(true);
+    setTimeout(() => {
+      setIsFirst(false);
+    }, 3000);
+  }, []);
+  if (isFirst) {
+    return (
+      <main
+        className={`${
+          isAnimate ? 'bg-main-black' : 'bg-dark-gray'
+        } fixed top-0 left-0 w-screen h-screen flex flex-col gap-10 items-center justify-center z-50 transition-2s`}
+      >
+        <p
+          className={`text-lg scale100 transition-2s opacity-100 color-black logo-font ${
+            isAnimate
+              ? 'transform scale-200 transition-2s opacity-0 color-white'
+              : ''
+          }`}
+        >
+          KEEBIT
+        </p>
+        {/* <p
+          className={`text-lg scale100 transition-2s opacity-100 color-black logo-font-2 ${
+            isAnimate
+              ? 'transform scale-200 transition-2s opacity-0 color-white'
+              : ''
+          }`}
+        >
+          KEEBIT
+        </p> */}
+      </main>
+    );
+  }
+};
