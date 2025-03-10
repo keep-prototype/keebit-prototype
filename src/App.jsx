0aeaef1d109762104e06af662a37b9818e42e9d9
csm0062 <csm0062@naver.com>
2025-03-10 10:20:03 +0900

    add: css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import './App.css'
 function App() {
 
   return (
-    <main></main>
+    <main className=''>
+      <img src="https://unicorn-images.b-cdn.net/62c76b76-fc3b-46fc-99a5-35151ff43bf6?optimizer=gif" alt="" />
+    </main>
   )
 }
 
