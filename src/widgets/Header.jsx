a8d8fd395ad77af56b618adcb73871bd9d3e2030
ganggyunggyu <qwzx16@naver.com>
2025-03-10 19:59:24 +0900

    feat: 헤더 백그라운드 분기 추가

--- src/widgets/Header.jsx
+++ src/widgets/Header.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 export const Header = ({ isMore, toggleMore }) => {
   const [isDelay, setIsDelay] = React.useState(false);
   const [isHamberger, setIsHamberger] = React.useState(true);
+  const [isHeaderBackground, setIsHeaderBackground] = React.useState(true);
+
   React.useEffect(() => {
     if (!isMore) {
       setIsDelay(false);
@@ -16,8 +18,23 @@ export const Header = ({ isMore, toggleMore }) => {
       }, 300);
     }
   }, [isMore]);
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY;
+      if (scrollY > 100) {
+        setIsHeaderBackground(false);
+      } else {
+        setIsHeaderBackground(true);
+      }
+    });
+  }, [window.scrollY]);
   return (
-    <header className="w-screen flex items-center justify-between fixed top-0 left-0 z-30 text-white p-4">
+    <header
+      className={`w-screen flex items-center justify-between fixed top-0 left-0 z-30 text-white p-4 transition-all ${
+        isHeaderBackground ? '' : ''
+      }`}
+    >
       <p className="text-lg">아파트 커뮤니티 예약 시스템</p>
       <button
         className="relative flex flex-col justify-center items-center gap-1.5 bg-white rounded-full h-10 w-10"
